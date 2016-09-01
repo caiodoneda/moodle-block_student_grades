c9bde5db4dc9d255554020e6fffa62356329a0b1
Caio Doneda <caiodoneda@gmail.com>
2016-08-31 22:32:41 -0300

    first commit

---
+++ block_student_grades.php
@@ -0,0 +1,69 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package    block_student_grades
+ * @copyright  Caio Doneda
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+include_once($CFG->dirroot . '/lib/enrollib.php');
+
+
+class block_student_grades extends block_base {
+    function init() {
+        $this->title = get_string('pluginname', 'block_student_grades');
+    }
+
+    function has_config() {
+        return true;
+    }
+
+    function get_content() {
+        global $CFG, $USER;
+
+        if($this->content !== NULL) {
+            return $this->content;
+        }
+
+        $this->content = new stdClass;
+        $this->content->text = "";
+        $this->content->footer = '';
+
+        $courses = enrol_get_users_courses($USER->id, true);        
+        $base_url = $CFG->wwwroot . '/grade/report/user/index.php?id=';
+
+        foreach ($courses as $c) {
+            $context = context_course::instance($c->id);
+            if (has_capability('moodle/grade:view', $context)) {
+                $this->content->text .= html_writer::link($base_url . $c->id, $c->fullname);
+                $this->content->text .= '<BR>';
+            }
+        }
+
+        if (!empty($this->content->text)) {
+            $this->content->text .= "<hr>";
+            $link = html_writer::link($CFG->wwwroot . "/blocks/student_grades/my_grades_resume.php", 
+                    get_string('resume', 'block_student_grades'));
+            $this->content->text .= $link;
+        }
+
+        return $this->content;
+    }
+}
+
+
---
+++ my_grades_resume.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package    block_student_grades
+ * @copyright  Caio Doneda
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+require_once(dirname(__FILE__).'/../../config.php');
+include_once($CFG->dirroot . '/lib/enrollib.php');
+include_once($CFG->dirroot . '/grade/querylib.php');
+
+
+$PAGE->set_url("/student_grades/my_grades_resume.php");
+$PAGE->set_title(get_string('resume', 'block_student_grades'));
+$PAGE->set_heading(get_string('resume', 'block_student_grades'));
+$PAGE->set_pagelayout('standard');
+$PAGE->navbar->add(get_string('resume', 'block_student_grades'));
+
+$PAGE->set_context(context_system::instance());
+
+require_login();
+
+echo $OUTPUT->header();
+
+$courses = enrol_get_users_courses($USER->id, true);
+
+$table = new html_table();
+$table->head  = array(get_string('course'), get_string('finalgrade', 'grades'));
+$table->data  = array();
+$table->attributes['class'] = 'generaltable';
+
+foreach ($courses as $c) {
+	$context = context_course::instance($c->id);
+    if (has_capability('moodle/grade:view', $context)) {
+		$grades = grade_get_course_grade($USER->id, $c->id);
+		$table->data[] = array($c->fullname, $grades->str_grade);
+	}
+}
+
+echo html_writer::table($table);
+
+echo $OUTPUT->footer();
